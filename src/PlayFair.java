f7fbf2e1cbd826706f0370c4893b71835ec7fe6e
Swan <swan.blanc.pro@gmail.com>
2019-09-13 12:11:15 +0200

    add pertubation

--- src/PlayFair.java
+++ src/PlayFair.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class PlayFair {
 
     private static final char[][] KEY = {{'t', 'q', 'o', 'h', 'w'},
@@ -10,8 +12,8 @@ public class PlayFair {
     }
 
     /**
-     * Obtien la position d'un caractere dans le tableau KEY
-     * @param c caractere a trouver
+     * Obtien la position d'un character dans le tableau KEY
+     * @param c character a trouver
      * @return la position y, x du character. null si il n'est pas dans le tableau
      */
     private int[] get_pos(char c) {
@@ -24,21 +26,69 @@ public class PlayFair {
     }
 
     /**
-     * Chiffre une paire de lettre
+     * Fait le modulo mathematique si chiffre < 0 il revient a b - 1
+     * @param a
+     * @param b
+     * @return a mod b
+     */
+    private int math_mod(int a, int b){
+        return ((a % b) + b) % b;
+    }
+
+    private int[] get_rand_pair(int min, int max){
+        int random1 = ThreadLocalRandom.current().nextInt(min, max);
+        int random2 = random1;
+        while (random1 == random2)
+            random2 = ThreadLocalRandom.current().nextInt(min, max);
+        return new int[]{random1, random2};
+    }
+
+    private char[][] permutation(int[] pos1, int[] pos2, char[][] t){
+        char temp = t[pos1[0]][pos1[1]];
+        t[pos1[0]][pos1[1]] = t[pos2[0]][pos2[1]];
+        t[pos2[0]][pos2[1]] = temp;
+        return t;
+    }
+
+    private char[][] perturbe_cle(char[][] key){
+
+        int[] pos1, pos2;
+        pos1 = get_rand_pair(0, KEY.length);
+        pos2 = pos1;
+        while (pos1[0] == pos2[0] && pos1[1] == pos2[1])
+            pos2 = get_rand_pair(0, KEY.length);
+
+        // Permutation de deux lettre
+        char[][]res = permutation(pos1, pos2, key);
+
+        // Permutation de deux ligne
+        char[] l_temp = res[pos1[0]];
+        res[pos1[0]] = res[pos2[0]];
+        res[pos2[0]] = l_temp;
+
+        // Permutation de deux colomne
+        for(int y=0; y < KEY.length; y++)
+            res = permutation(new int[]{y, pos1[1]}, new int[]{y, pos2[1]}, res);
+        return res;
+
+    }
+
+    /**
+     * Chiffrer une paire de lettre
      * @param pos1 list position dans le tableau KEY (y, x)
      * @param pos2 list position dans le tableau KEY (y, x)
-     * @return les deux lettres chiffrees
+     * @return les deux lettre chiffrer
      */
     private String chiffre_paire(int[] pos1, int[] pos2) {
 
         String str = "";
-        // Si c'est la meme ligne
+        // Si c'est la maime ligne
         if (pos1[0] == pos2[0]) {
             int x1, x2;
             x1 = math_mod(pos1[1] + 1, KEY[pos1[0]].length);
             x2 = math_mod(pos2[1] + 1, KEY[pos2[0]].length);
             return str + KEY[pos1[0]][x1] + KEY[pos2[0]][x2];
-        // Si c'est la meme ligne
+        // Si c'est la maime ligne
         } else if (pos1[1] == pos2[1]) {
             int y1, y2;
             y1 = math_mod(pos1[0] + 1, KEY.length);
@@ -50,26 +100,22 @@ public class PlayFair {
         }
     }
 
-    private int math_mod(int a, int b){
-        return ((a % b) + b) % b;
-    }
-
     /**
      * dechiffre une paire de lettre
      * @param pos1 list position dans le tableau KEY (y, x)
      * @param pos2 list position dans le tableau KEY (y, x)
-     * @return les deux lettres dechiffrees
+     * @return les deux lettre dechiffre
      */
     private String dechiffre_paire(int[] pos1, int[] pos2) {
 
         String str = "";
-        // Si c'est la meme ligne
+        // Si c'est la maime ligne
         if (pos1[0] == pos2[0]) {
             int x1, x2;
             x1 = math_mod(pos1[1] - 1, KEY[pos1[0]].length);
             x2 = math_mod(pos2[1] - 1, KEY[pos2[0]].length);
             return str + KEY[pos1[0]][x1] + KEY[pos2[0]][x2];
-        // Si c'est la meme ligne
+        // Si c'est la maime ligne
         } else if (pos1[1] == pos2[1]) {
             int y1, y2;
             y1 = math_mod(pos1[0] - 1, KEY.length);
@@ -84,8 +130,8 @@ public class PlayFair {
     /**
      * Chiffre un texte
      * @param text
-     * @return le texte chiffre
-     * @throws Exception Si il y a des lettres qui ne sont pas dans le tableau KEY
+     * @return le texte chiffrer
+     * @throws Exception Si il y a des lettre qui ne sont pas dans la tableau KEY
      */
     public String chiffre_texte(String text) throws Exception {
 
@@ -98,7 +144,7 @@ public class PlayFair {
             int[] pos2 = get_pos(text.charAt(i + 1));
             if (pos1 == null || pos2 == null) {
                 throw new Exception("l1 or l2 not in the key tab");
-            // Si les deux char son egaux on rajoute x
+            // Si les deux char son egaut on rajoute x
             } else if (text.charAt(i) == text.charAt(i + 1)) {
                 res.append(text.charAt(i)).append('x').append(text.charAt(i + 1));
             } else {
@@ -111,8 +157,8 @@ public class PlayFair {
     /**
      * dechiffre un texte
      * @param text
-     * @return le texte dechiffree
-     * @throws Exception Si il y a des lettres qui ne sont pas dans le tableau KEY
+     * @return le texte dechiffre
+     * @throws Exception Si il y a des lettre qui ne sont pas dans la tableau KEY
      */
     public String dechiffre_texte(String text) throws Exception {
 
@@ -122,7 +168,7 @@ public class PlayFair {
             int[] pos2 = get_pos(text.charAt(i + 1));
             if (pos1 == null || pos2 == null) {
                 throw new Exception("l1 or l2 not in the key tab");
-            // Si les deux char suivant sont un x on suppose qu'il a ete rajouter donc on le retire
+            // Si les deux char suivent est un x on supose qu'il a ete rajouter donc on le retire
             } else if (text.charAt(i + 1) == 'x') {
                 res.append(text.charAt(i)).append(text.charAt(i + 2));
                 i += 1;
