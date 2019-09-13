bd292c3ee859a4034adfc429a234a6de25f9f539
Swan <swan.blanc.pro@gmail.com>
2019-09-13 11:27:48 +0200

    Add playfair

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 public class Main {
     public static void main(String args[]) {
 
-        System.out.println("Hello");
-    }
+        PlayFair playFair = new PlayFair();
 
-    public Main() {
+        try {
+            String chiffre = playFair.chiffre_texte("lechattonestla");
+            String dechiffre = playFair.dechiffre_texte(chiffre);
+            System.out.println(chiffre);
+            System.out.println(dechiffre);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
